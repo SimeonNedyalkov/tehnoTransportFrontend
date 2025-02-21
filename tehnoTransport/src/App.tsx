fd62e5f26800de7cb10f0c66f53068c145a2cff7
SimeonNedyalkov <simeon_sg@abv.bg>
2025-02-21 21:47:13 +0200

    Login created

--- tehnoTransport/src/App.tsx
+++ tehnoTransport/src/App.tsx
@@ -1,13 +1,15 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+// import reactLogo from "./assets/react.svg";
+// import viteLogo from "/vite.svg";
+import Login from "./user/Login";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
+      <Login />
+      {/* <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -26,7 +28,7 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p>
+      </p> */}
     </>
   );
 }
---
+++ tehnoTransport/src/user/Login.tsx
@@ -0,0 +1,118 @@
+import { Checkbox, Field, Label } from "@headlessui/react";
+import { useState } from "react";
+import googleIco from "../../assets/signupwith/google-logo-search-new-svgrepo-com.svg";
+import facebookIco from "../../assets/signupwith/facebook-1-svgrepo-com.svg";
+import instagramIco from "../../assets/signupwith/instagram-svgrepo-com.svg";
+// import useForm from "../hooks/useForm";
+// import usersAPI from "../../services/usersAPI";
+// import { useNavigate } from "react-router-dom";
+// import { useAuthContext } from "../../contexts/UserContext";
+const initialValues = { email: "", password: "" };
+export default function Login() {
+  const [enabled, setEnabled] = useState(false);
+  const [error, setError] = useState("");
+  //   const { changeAuthState } = useAuthContext();
+  //   const navigation = useNavigate();
+  //   const { values, submitHandler, changeHandler } = useForm(
+  //     initialValues,
+  //     async () => {
+  //       const { email, password } = values;
+  //       try {
+  //         const accessToken = await usersAPI.login(email, password);
+  //         if (!accessToken) {
+  //           throw new Error("Login failed: Invalid credentials.");
+  //         }
+
+  //         const userStatus = await usersAPI.getStatus(accessToken);
+  //         const userData = await userStatus.json();
+
+  //         const updatedAuthState = {
+  //           user: {
+  //             email: userData.email,
+  //             _id: userData._id,
+  //             accessToken,
+  //           },
+  //         };
+
+  //         changeAuthState(updatedAuthState);
+  //         navigation("/");
+  //       } catch (err: any) {
+  //         setError(err.message || "Something went wrong. Please try again.");
+  //       }
+  //     }
+  //   );
+  return (
+    <>
+      <div className="loginForm">
+        <div className="formWrapper">
+          <form className="flex flex-col">
+            <h1 className="text-4xl text-sky-950 pb-5">
+              Tehno Transport Admin Login.
+            </h1>
+            <h5 className="text-sm text-gray-600 pb-5">
+              This portal is for Tehno Transport staff only. Unauthorized access
+              is prohibited.
+            </h5>
+            {error && <div className="text-lg pb-5">{error}</div>}
+            <div className="mb-3 flex flex-col">
+              <label className="flex justify-start styledEmail">E-mail</label>
+              <input
+                type="email"
+                className="form-control styledInput"
+                placeholder="example@email.com"
+                name="email"
+              />
+            </div>
+
+            <div className="mb-3 flex flex-col">
+              <label className="flex justify-start styledPassword">
+                Password
+              </label>
+              <input
+                type="password"
+                className="form-control styledInput"
+                placeholder="Your Password"
+                name="password"
+              />
+            </div>
+
+            <div className="mb-3 mt-3 flex flex-row items-center justify-between">
+              <Field className="flex items-center gap-2">
+                <Checkbox
+                  checked={enabled}
+                  onChange={setEnabled}
+                  className="group block size-4 rounded border bg-white data-[checked]:bg-blue-500"
+                >
+                  <svg
+                    className="stroke-white opacity-0 group-data-[checked]:opacity-100"
+                    viewBox="0 0 14 14"
+                    fill="none"
+                  >
+                    <path
+                      d="M3 8L6 11L11 3.5"
+                      strokeWidth={2}
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                    />
+                  </svg>
+                </Checkbox>
+                <Label>Remember me</Label>
+              </Field>
+              <p className="forgot-password text-right">
+                Forgot <a href="#">password?</a>
+              </p>
+            </div>
+            <div className="divider">
+              <hr className="line" />
+            </div>
+            <div className="d-grid flex justify-center">
+              <button type="submit" className="loginBtn">
+                Login
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </>
+  );
+}
