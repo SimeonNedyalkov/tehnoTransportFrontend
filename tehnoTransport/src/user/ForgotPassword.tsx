a56a83be680bc7a02cfdfe25ff8344a2c6c95946
SimeonNedyalkov <simeon_sg@abv.bg>
2025-03-25 17:29:38 +0200

    styles added

--- tehnoTransport/src/user/ForgotPassword.tsx
+++ tehnoTransport/src/user/ForgotPassword.tsx
@@ -31,35 +31,73 @@ export default function ForgotPassword() {
     <Flex
       direction="column"
       justifyContent="center"
-      alignContent="center"
       alignItems="center"
+      bg="gray.50"
+      minH="100vh"
+      py="8"
     >
-      <Heading>Forgot Your Password ??? </Heading>
-      <Heading>Send me an email !!! </Heading>
+      <Heading as="h1" size="xl" color="teal.600" mb="4">
+        Forgot Your Password?
+      </Heading>
+      <Heading as="h2" size="md" color="gray.500" mb="8">
+        We'll send you an email to reset it!
+      </Heading>
       <Box
-        maxW="md"
-        mx="auto"
-        mt={{ base: "4", md: "12" }}
-        p={{ base: "6", md: "8" }}
-        borderRadius="lg"
-        boxShadow="lg"
+        maxW="lg"
+        w="full"
+        p="8"
+        borderRadius="md"
         bg="white"
+        boxShadow="lg"
+        border="1px"
+        borderColor="gray.200"
       >
         <form onSubmit={onSubmit}>
-          <Stack gap="4" align="flex-start" maxW="sm">
+          <Stack gap="6" align="flex-start">
             <Field.Root invalid={!!errors.email}>
-              <Field.Label>Email</Field.Label>
-              <Input {...register("email")} />
+              <Field.Label htmlFor="email">Email</Field.Label>
+              <Input
+                id="email"
+                type="email"
+                placeholder="Enter your email"
+                size="lg"
+                {...register("email", {
+                  required: "Email is required",
+                  pattern: {
+                    value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/,
+                    message: "Please enter a valid email",
+                  },
+                })}
+              />
               <Field.ErrorText>{errors.email?.message}</Field.ErrorText>
             </Field.Root>
 
             <Field.Root invalid={!!errors.password}>
-              <Field.Label>Last name</Field.Label>
-              <PasswordInput {...register("password")} />
+              <Field.Label htmlFor="password">Password</Field.Label>
+              <PasswordInput
+                id="password"
+                placeholder="Enter new password"
+                size="lg"
+                {...register("password", {
+                  required: "Password is required",
+                  minLength: {
+                    value: 6,
+                    message: "Password must be at least 6 characters long",
+                  },
+                })}
+              />
               <Field.ErrorText>{errors.password?.message}</Field.ErrorText>
             </Field.Root>
 
-            <Button type="submit">Submit</Button>
+            <Button
+              type="submit"
+              size="lg"
+              colorScheme="teal"
+              width="full"
+              mt="4"
+            >
+              Send Reset Link
+            </Button>
           </Stack>
         </form>
       </Box>
