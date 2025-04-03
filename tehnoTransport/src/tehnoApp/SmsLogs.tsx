d95a925d43f32bc0b77aab27a4ae693bca4e37f3
SimeonNedyalkov <simeon_sg@abv.bg>
2025-04-03 15:41:41 +0300

    UI change

--- tehnoTransport/src/tehnoApp/SmsLogs.tsx
+++ tehnoTransport/src/tehnoApp/SmsLogs.tsx
@@ -1,7 +1,17 @@
-import { Avatar, Badge, Button, Card, HStack, Stack } from "@chakra-ui/react";
+import {
+  Avatar,
+  Badge,
+  Button,
+  Card,
+  Flex,
+  HStack,
+  Stack,
+  Text,
+} from "@chakra-ui/react";
 import useGetSmsLogs from "../hooks/useGetSmsLogs";
 import { useEffect, useState } from "react";
 import SmsInterface from "../interfaces/SmsInterface";
+import timestampToDateStringConverter from "../tools/DateOrTimestampConverter";
 
 export default function SmsLogs() {
   const DATA = useGetSmsLogs();
@@ -19,14 +29,36 @@ export default function SmsLogs() {
       {smses.map((sms, index) => (
         <Card.Root width="320px" variant="elevated" key={index}>
           <Card.Body gap="2">
-            <Card.Title mb="2">{sms.senderName || "Unknown Sender"}</Card.Title>
-            <Avatar.Root size="lg" shape="rounded">
-              <Avatar.Image src={"https://picsum.photos/200/300"} />
-              <Avatar.Fallback name={sms.senderName || "Unknown Sender"} />
-            </Avatar.Root>
-            <HStack mt="4">
-              <Badge>Sender: {sms.senderName}</Badge>
-              <Badge>Receiver: {sms.receiverName}</Badge>
+            <Flex justify="space-between" align="center">
+              <HStack mt="4" justifyContent="space-between">
+                <Avatar.Root size="lg" shape="rounded">
+                  <Avatar.Image src={"https://picsum.photos/200/300"} />
+                  <Avatar.Fallback name={sms.senderName || "Unknown Sender"} />
+                </Avatar.Root>
+                <Card.Title mb="2">
+                  {sms.senderName || "Unknown Sender"}
+                </Card.Title>
+              </HStack>
+            </Flex>
+            <HStack justifyContent="space-between">
+              <Text color="fg.muted" textStyle="sm">
+                @
+                {String(
+                  timestampToDateStringConverter(sms.sentAt)
+                    .toISOString()
+                    .split("T")[0]
+                )}
+              </Text>
+
+              {sms.response == "success" ? (
+                <Text color="green.600" textStyle="sm">
+                  {sms.response}
+                </Text>
+              ) : (
+                <Text color="red.600" textStyle="sm">
+                  {sms.response}
+                </Text>
+              )}
             </HStack>
             <Card.Description>
               {sms.message.length > 50
@@ -35,8 +67,10 @@ export default function SmsLogs() {
             </Card.Description>
           </Card.Body>
           <Card.Footer justifyContent="flex-end">
-            <Button variant="outline">View</Button>
-            <Button>Reply</Button>
+            <HStack mt="4">
+              <Badge>Sender: {sms.senderName}</Badge>
+              <Badge>Receiver: {sms.receiverName}</Badge>
+            </HStack>
           </Card.Footer>
         </Card.Root>
       ))}
