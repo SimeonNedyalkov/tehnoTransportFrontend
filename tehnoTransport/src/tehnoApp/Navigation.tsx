e578d39962cca1240e02e883ff86839435480ac6
SimeonNedyalkov <simeon_sg@abv.bg>
2025-04-21 16:17:39 +0300

    c

--- tehnoTransport/src/tehnoApp/Navigation.tsx
+++ tehnoTransport/src/tehnoApp/Navigation.tsx
@@ -47,11 +47,7 @@ export default function Navigation() {
       </div>
       <MenuItems
         transition
-        className={
-          isDark
-            ? "absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 ring-1 shadow-lg ring-black/5 transition focus:outline-hidden data-closed:scale-95 data-closed:transform data-closed:opacity-0 data-enter:duration-100 data-enter:ease-out data-leave:duration-75 data-leave:ease-in"
-            : "absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-black py-1 ring-1 shadow-lg ring-black/5 transition focus:outline-hidden data-closed:scale-95 data-closed:transform data-closed:opacity-0 data-enter:duration-100 data-enter:ease-out data-leave:duration-75 data-leave:ease-in"
-        }
+        className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md py-1 ring-1 shadow-lg ring-black/5 transition focus:outline-hidden data-closed:scale-95 data-closed:transform data-closed:opacity-0 data-enter:duration-100 data-enter:ease-out data-leave:duration-75 data-leave:ease-in"
       >
         <MenuItem>
           <Link
